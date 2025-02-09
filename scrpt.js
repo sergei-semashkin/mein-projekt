e2f9935926d5bb3e3cccbc3ee77fb34d6adb52ad
sergei-semashkin <sergei.semashkin@web.de>
2025-02-09 18:35:02 +0100

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,13 @@
+const a = [1,2,'a','b'];
+const filter_list1 = [1,'a','b',0,15];
+const filter_list3 = [1,2,'aasf','1','123',123];
+
+
+let newFilter = a.filter(item => a.item.typeof === 'string');
+
+console.log(newFilter)
+
+//console.log(JSON.stringify(arr.filter(item => typeof item === "number")));
+
+
+
